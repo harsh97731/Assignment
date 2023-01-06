0a15737482d7abcd06b651ad9f236155c25750be
Zignuts Technolab <ztlab100@Zignutss-Mac-mini.local>
2023-01-06 15:52:20 +0530

    add firebase google signin

--- lib/DashBoard.dart
+++ lib/DashBoard.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:loginsin/ChangedPassword.dart';
+import 'package:loginsin/Google_Sign_In.dart';
 import 'package:loginsin/LoginPage.dart';
 
 class DashBord extends StatefulWidget {
@@ -13,33 +15,47 @@ class DashBord extends StatefulWidget {
 
 class _DashBordState extends State<DashBord> {
   int index = 0;
-  List<Widget> Screens=[
+  String? UserEmail = FirebaseAuth.instance.currentUser?.email;
+
+  List<Widget> Screens = [
     Center(child: Text("My home page")),
     Center(child: Text("My home pa")),
-  Center(child: Text("My home page")),
- Center(child: Text("My home page")),
- Center(child: Text("My home page"))];
+    Center(child: Text("My home page")),
+    Center(child: Text("My home page")),
+    Center(child: Text("My home page"))
+  ];
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-      onWillPop: () async{
-        final value = await showDialog<bool>(context: context, builder: (context) {
-          return AlertDialog(
-              title: Text('Alert'),
-              content:  Text('Do you want to exit app?'),
-              actions: [
-                ElevatedButton(onPressed: ()=>Navigator.of(context).pop(false),style: ElevatedButton.styleFrom(backgroundColor: Colors.black, foregroundColor: Colors.white), child: Text('NO')),
-                ElevatedButton(onPressed: ()=>Navigator.of(context).pop(true), style: ElevatedButton.styleFrom(backgroundColor: Colors.black, foregroundColor: Colors.white), child: Text('Exit')),
-              ]);
-        });
-        if(value!= null){
+      onWillPop: () async {
+        final value = await showDialog<bool>(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                  title: Text('Alert'),
+                  content: Text('Do you want to exit app?'),
+                  actions: [
+                    ElevatedButton(
+                        onPressed: () => Navigator.of(context).pop(false),
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.black,
+                            foregroundColor: Colors.white),
+                        child: Text('NO')),
+                    ElevatedButton(
+                        onPressed: () => Navigator.of(context).pop(true),
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.black,
+                            foregroundColor: Colors.white),
+                        child: Text('Exit')),
+                  ]);
+            });
+        if (value != null) {
           return Future.value(value);
-        }else{
+        } else {
           return Future.value(false);
         }
       },
       child: Scaffold(
-
         body: Screens[index],
         drawer: Drawer(
           child: ListView(
@@ -55,10 +71,11 @@ class _DashBordState extends State<DashBord> {
                     "Harsh vaddoriya",
                     style: TextStyle(fontSize: 18),
                   ),
-                  accountEmail: Text("Harshvaddoriya2550@gmail.com"),
+                  accountEmail: Text("h"),
                   currentAccountPictureSize: Size.square(50),
                   currentAccountPicture: CircleAvatar(
                     backgroundColor: Color.fromARGB(255, 255, 251, 251),
+
                     child: Text(
                       "H",
                       style: TextStyle(fontSize: 30.0, color: Colors.black),
@@ -88,10 +105,14 @@ class _DashBordState extends State<DashBord> {
                 },
               ),
               ListTile(
-                leading: const Icon(Icons.video_label),
-                title: const Text(' Password '),
+                leading: const Icon(Icons.password_sharp),
+                title: const Text(' Change Password '),
                 onTap: () {
-                  Navigator.pop(context);
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ChangedPassword(),
+                      ));
                 },
               ),
               ListTile(
@@ -101,33 +122,43 @@ class _DashBordState extends State<DashBord> {
                   Navigator.pop(context);
                 },
               ),
+
               ListTile(
                 leading: const Icon(Icons.logout),
                 title: const Text('LogOut'),
                 onTap: () {
-
-                  showDialog(context: context, builder: (context) {
-                    return Container(
-                      child: AlertDialog(
-                        backgroundColor: Colors.white,
-                        title: Text("Are you sure ?"),
-                        actions: [
-                          TextButton(onPressed: ()async{
-                            if (FirebaseAuth.instance.currentUser !=
-                                null) {
-                              await FirebaseAuth.instance.signOut();
-                            }
-                            Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => LoginPage(),), (route) => false);
-                          }, child: Text("yes")),
-                          TextButton(onPressed: (){
-                            Navigator.pop(context);
-                          }, child: Text("no"))
-                        ],
-
-
-                      ),
-                    );
-                  },);
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return Container(
+                        child: AlertDialog(
+                          backgroundColor: Colors.white,
+                          title: Text("Are you sure ?"),
+                          actions: [
+                            TextButton(
+                                onPressed: () async {
+                                  if (FirebaseAuth.instance.currentUser !=
+                                      null) {
+                                    await Google_Sign_In().signOut();
+                                  }
+                                  Navigator.pushAndRemoveUntil(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => LoginPage(),
+                                      ),
+                                      (route) => false);
+                                },
+                                child: Text("yes")),
+                            TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Text("no"))
+                          ],
+                        ),
+                      );
+                    },
+                  );
                   ;
                 },
               ),
@@ -136,28 +167,42 @@ class _DashBordState extends State<DashBord> {
         ),
         appBar: AppBar(
           backgroundColor: Colors.black,
-          title: Text('Dashboard',
-            ),
+          title: Text(
+            'Dashboard',
+          ),
         ),
         bottomNavigationBar: GNav(
-            padding: EdgeInsets.only(left: 10, right: 10,bottom: 10),
-
-
+          padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
           onTabChange: (val) {
-            currentIndex: index;
+            currentIndex:
+            index;
 
             setState(() {
-              index=val;
+              index = val;
             });
           },
-
           gap: 2,
-          tabs: const[
-            GButton(icon: Icons.home,text: 'Home',),
-            GButton(icon: Icons.people,text: 'Vendors',),
-            GButton(icon: Icons.list,text: 'List',),
-            GButton(icon: Icons.category,text: 'Category',),
-            GButton(icon: Icons.menu_open,text: 'Menu',),
+          tabs: const [
+            GButton(
+              icon: Icons.home,
+              text: 'Home',
+            ),
+            GButton(
+              icon: Icons.people,
+              text: 'Vendors',
+            ),
+            GButton(
+              icon: Icons.list,
+              text: 'List',
+            ),
+            GButton(
+              icon: Icons.category,
+              text: 'Category',
+            ),
+            GButton(
+              icon: Icons.menu_open,
+              text: 'Menu',
+            ),
           ],
         ),
       ),
--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -2,12 +2,13 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:loginsin/DashBoard.dart';
 import 'package:loginsin/ForgotPassword.dart';
+import 'package:loginsin/Google_Sign_In.dart';
 import 'package:loginsin/sigup.dart';
 
 class LoginPage extends StatefulWidget {
-
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
@@ -17,204 +18,256 @@ class _LoginPageState extends State<LoginPage> {
   GlobalKey<FormState> formkey = GlobalKey<FormState>();
 
   // text controller
-    final _emailControler =TextEditingController();
+  final _emailControler = TextEditingController();
 
-    final _passwordController = TextEditingController();
-
+  final _passwordController = TextEditingController();
 
   bool _isObscure = true;
-  bool isLoading =false;
+  bool isLoading = false;
+
 
   @override
   Widget build(BuildContext context) {
     return Form(
       key: formkey,
       child: WillPopScope(
-        onWillPop: () async{
-        final value = await showDialog<bool>(context: context, builder: (context) {
-            return AlertDialog(
-              title: Text('Alert'),
-              content:  Text('Do you want to exit app?'),
-              actions: [
-                ElevatedButton(onPressed: ()=>Navigator.of(context).pop(false),style: ElevatedButton.styleFrom(backgroundColor: Colors.black, foregroundColor: Colors.white), child: Text('NO')),
-                ElevatedButton(onPressed: ()=>Navigator.of(context).pop(true), style: ElevatedButton.styleFrom(backgroundColor: Colors.black, foregroundColor: Colors.white), child: Text('Exit')),
-            ]);
-          });
-        if(value!= null){
-          return Future.value(value);
-        }else{
-          return Future.value(false);
-        }
+        onWillPop: () async {
+          final value = await showDialog<bool>(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                    title: Text('Alert'),
+                    content: Text('Do you want to exit app?'),
+                    actions: [
+                      ElevatedButton(
+                          onPressed: () => Navigator.of(context).pop(false),
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.black,
+                              foregroundColor: Colors.white),
+                          child: Text('NO')),
+                      ElevatedButton(
+                          onPressed: () => Navigator.of(context).pop(true),
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.black,
+                              foregroundColor: Colors.white),
+                          child: Text('Exit')),
+                    ]);
+              });
+          if (value != null) {
+            return Future.value(value);
+          } else {
+            return Future.value(false);
+          }
         },
         child: Scaffold(
           backgroundColor: Colors.white,
-            body: SingleChildScrollView(
-              child: Column(
-                children: <Widget>[
-                  SizedBox(width: 70,height: 70,),
-                  Center(
-                      child: Icon(Icons.login_outlined,size: 70,)
+          body: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  width: 70,
+                  height: 70,
+                ),
+                Center(
+                    child: Icon(
+                  Icons.login_outlined,
+                  size: 70,
+                )),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'Login',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
                   ),
-                  SizedBox(height: 20,),
-                  Text('Login',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,fontSize: 20,
-                  ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-
-                    child: TextFormField(
-                      autovalidateMode: AutovalidateMode.onUserInteraction,
-                      controller: _emailControler,
-
-                      decoration: InputDecoration(
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextFormField(
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    controller: _emailControler,
+                    decoration: InputDecoration(
                         label: Text('Email'),
                         icon: Icon(Icons.mail_lock_outlined),
-                          errorBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.black26)),
-                          focusedErrorBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.black26)),
-                          enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.black26)),
-                          focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.black26))
-
-
-                      ),
-    validator: (value) {
-    if (value == null || value.trim().isEmpty) {
-    return 'Please enter your email address';
-    }
-    // Check if the entered email has the right format
-    if (!RegExp(r'\S+@\S+\.\S+').hasMatch(value)) {
-    return 'Please enter a valid email address';
-    }
-    // Return null if the entered email is valid
-    return null;
-    },
-
-                    ),
+                        errorBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black26)),
+                        focusedErrorBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black26)),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black26)),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black26))),
+                    validator: (value) {
+                      if (value == null || value.trim().isEmpty) {
+                        return 'Please enter your email address';
+                      }
+                      // Check if the entered email has the right format
+                      if (!RegExp(r'\S+@\S+\.\S+').hasMatch(value)) {
+                        return 'Please enter a valid email address';
+                      }
+                      // Return null if the entered email is valid
+                      return null;
+                    },
                   ),
-                  SizedBox(height: 20,),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: TextFormField(
-                      autovalidateMode: AutovalidateMode.onUserInteraction,
-                      obscureText: _isObscure,
-                      controller: _passwordController,
-                      decoration: InputDecoration(
-                        
-                        label: Text('Password',),
-                        icon: Icon(Icons.password_outlined,),
-                          errorBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.black26)),
-                          focusedErrorBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.black26)),
-                        enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.black26)),
-                        focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.black26)),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextFormField(
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    obscureText: _isObscure,
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                        label: Text(
+                          'Password',
+                        ),
+                        icon: Icon(
+                          Icons.password_outlined,
+                        ),
+                        errorBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black26)),
+                        focusedErrorBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black26)),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black26)),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black26)),
                         suffixIcon: IconButton(
-                          icon: Icon(_isObscure ? Icons.visibility :Icons.visibility_off),
+                          icon: Icon(_isObscure
+                              ? Icons.visibility
+                              : Icons.visibility_off),
                           onPressed: () {
-
-                            setState(() { //refresh UI
-                              if(_isObscure){ //if passenable == true, make it false
+                            setState(() {
+                              //refresh UI
+                              if (_isObscure) {
+                                //if passenable == true, make it false
                                 _isObscure = false;
-                              }else{
-                                _isObscure = true; //if passenable == false, make it true
+                              } else {
+                                _isObscure =
+                                    true; //if passenable == false, make it true
                               }
                             });
-                        },
-                        )
-
-
-
-                      ),
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return "Required";
-                        }
-                        if (value.length < 6) {
-                          return "Password must be atleast 6 characters long";
-                        }
-                        if (value.length > 20) {
-                          return "Password must be less than 20 characters";
-                        }
-                        if (!value.contains(RegExp(r'[0-9]'))) {
-                          return "Password must contain a number";
-                        }
-                      },
-                    ),
+                          },
+                        )),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Required";
+                      }
+                      if (value.length < 6) {
+                        return "Password must be atleast 6 characters long";
+                      }
+                      if (value.length > 20) {
+                        return "Password must be less than 20 characters";
+                      }
+                      if (!value.contains(RegExp(r'[0-9]'))) {
+                        return "Password must contain a number";
+                      }
+                    },
                   ),
-
-
-                  SizedBox(
-                    height: 1,
-                  ),
-          Container(
-              margin: EdgeInsets.all(0),
-              child: ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.black),
                 ),
-                child: Text('LogIn', style: TextStyle(fontSize: 20.0),),
-
-
-                onPressed: () async{
-                  if (formkey.currentState!.validate()) {
-                    setState(() {
-                      isLoading =true;
-                    });
-                    try
-                    {
-                      UserCredential userCredential = await FirebaseAuth
-                          .instance
-                          .signInWithEmailAndPassword(
-                          email: _emailControler.text.trim(),
-                          password: _passwordController.text.trim());
-                      if (userCredential.user != null) {
-                        Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => DashBord()));
+                SizedBox(
+                  height: 1,
+                ),
+                Container(
+                  margin: EdgeInsets.all(0),
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.black),
+                    ),
+                    child: Text(
+                      'LogIn',
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                    onPressed: () async {
+                      if (formkey.currentState!.validate()) {
+                        setState(() {
+                          isLoading = true;
+                        });
+                        try {
+                          UserCredential userCredential = await FirebaseAuth
+                              .instance
+                              .signInWithEmailAndPassword(
+                                  email: _emailControler.text.trim(),
+                                  password: _passwordController.text.trim());
+                          if (userCredential.user != null) {
+                            Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => DashBord()));
+                          }
+                        } on FirebaseAuthException catch (e) {
+                          if (e.code == 'user-not-found') {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(content: Text('user not found')));
+                          }
+                        }
                       }
-
-                    }
-                    on FirebaseAuthException catch(e){
-                      if(e.code=='user-not-found'){
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('user not found')));
-                      }
-
-                    }
-
-                  }
-                },
-              ),
-          ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text('you dont have an account?',
+                    },
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'you dont have an account?',
                       style: TextStyle(
                         color: Colors.black54,
                       ),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => SignUp(),));
-                        },
-                        child: Text('signup'),
-                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => SignUp(),
+                            ));
+                      },
+                      child: Text('signup'),
+                    ),
+                  ],
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ForgotPassword(),
+                        ));
+                  },
+                  child: Text('Forgot password?'),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                isLoading
+                    ? SpinKitCircle(
+                        color: Colors.black,
+                        size: 30,
+                      )
+                    : SizedBox(),
+                ElevatedButton(
 
-                    ],
-
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.black)
+                    ,
                   ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => ForgotPassword(),));
-                    },
-                    child: Text('Forgot password?'),
+                  child: Text(
+                    'Login with Google',
+                    style: TextStyle(fontSize: 20.0),
                   ),
-                    SizedBox(height: 20,),
-                  isLoading? SpinKitCircle(color:Colors.black,size: 30,) : SizedBox(),
+                  onPressed: ()async{
+                    await Google_Sign_In().signInWithGoogle();
+                    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => DashBord(),));
 
-                ],
-              ),
+                  },
+                ),
+              ],
             ),
+          ),
         ),
       ),
     );
